85a1c8ca1251bfea61c8aa1ff836af1ddced5a70
MUTWIRI0922 <amutwiri07@gmail.com>
2026-02-21 00:47:14 +0300

    restricted viewsets for access only by authenticated users

--- Rentals/views.py
+++ Rentals/views.py
@@ -1,18 +1,25 @@
 from django.shortcuts import render
 from .models import Tenant, Room, Lease
 from rest_framework import viewsets
+from rest_framework import permissions
+from rest_framework.decorators import api_view
+
+
 from .serializers import TenantSerializer, RoomSerializer, LeaseSerializer  
 
 # Create your views here.
 class TenantViewSet(viewsets.ModelViewSet):
+    permission_classes = [permissions.IsAuthenticated]
     queryset = Tenant.objects.all()
     serializer_class = TenantSerializer
-    
+
 
 class RoomViewSet(viewsets.ModelViewSet):
+    permission_classes = [permissions.IsAuthenticated]
     queryset = Room.objects.all()
     serializer_class = RoomSerializer
 
 class LeaseViewSet(viewsets.ModelViewSet):
+    permission_classes = [permissions.IsAuthenticated]
     queryset = Lease.objects.all()
     serializer_class = LeaseSerializer
