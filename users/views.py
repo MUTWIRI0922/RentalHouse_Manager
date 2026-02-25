cbc19c576ad62fbfa9ae6c57237a9a969b46123f
MUTWIRI0922 <amutwiri07@gmail.com>
2026-02-25 22:37:25 +0300

    created user regisration API

--- users/views.py
+++ users/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+from .models import CustomUser
+from rest_framework import viewsets
+from rest_framework import permissions
+from .serializers import CustomUserSerializer
+
+class RegisterApiView(generics.CreateApiView)
+    queryset = CustomUser.objects.all()
+    serializer_class = CustomUserSerializer
+    permission_classes = [permissions.AllowAny]
+
+    def post(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.save()
+
+
+
+
+
+
